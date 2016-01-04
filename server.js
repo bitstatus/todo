57548cc4b557376a355900ece8229072ea125eb7
venkat <venkyoct6@gmail.com>
2016-01-04 17:54:41 +0530

    Initilize repositary

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId =1;
+app.use(bodyParser.json());
+app.get('/', function(req,res){
+   res.send('Home');
+});
+app.get('/todos', function(req,res){
+	res.json(todos);
+});
+app.get('/todos/:id',function(req, res){
+	var todoid = parseInt(req.params.id, 10);
+	var matchid;
+	todos.forEach(function(tood){
+		if(todoid===tood.id){
+		matchid==tood;
+}
+	});
+	if(matchid){
+		res.json(matchid);
+	}else{
+		res.status(404).send();
+	}
+	
+ 
+});
+app.post('/todos', function(req,res){
+var body = req.body;
+body.id = todoNextId++;
+todos.push(body);
+res.json(body);
+});
+app.listen(PORT, function(){
+	console.log('server lisitining in' +PORT);
+});
