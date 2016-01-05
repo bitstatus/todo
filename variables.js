d7ebcbc7c1c8d13d5b5153194919738a9a8a6bd3
venkat <venkyoct6@gmail.com>
2016-01-05 16:52:18 +0530

    Added varibles.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+var person = ['venkat','raji']
+function newobj(obj){
+    person.push('lavanya')
+}
+function newobj(obj){
+    obj.push('lavanya');
+    debugger;
+}
+console.log(person);
+newobj(person);
+console.log(person);
